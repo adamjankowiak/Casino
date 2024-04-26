b5c2cf2ecd9b982c3c451e9f12e250c0d0f0a41a
Stacjonarka <mjwoj@DESKTOP-4R65IHO>
2024-04-26 22:38:13 +0200

    Dodanie tury zmiana  wyglądu i zmiana wyświetalnia talii

--- Gapa.Designer.cs
+++ Gapa.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.current_player = new System.Windows.Forms.Label();
+            this.current_deck = new System.Windows.Forms.Label();
+            this.playerLeft = new System.Windows.Forms.Label();
+            this.playerTop = new System.Windows.Forms.Label();
+            this.playerRight = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // current_player
+            // 
+            this.current_player.AutoSize = true;
+            this.current_player.BackColor = System.Drawing.Color.Beige;
+            this.current_player.Location = new System.Drawing.Point(369, 330);
+            this.current_player.Name = "current_player";
+            this.current_player.Size = new System.Drawing.Size(35, 13);
+            this.current_player.TabIndex = 0;
+            this.current_player.Text = "label1";
+            // 
+            // current_deck
+            // 
+            this.current_deck.AutoSize = true;
+            this.current_deck.BackColor = System.Drawing.Color.Beige;
+            this.current_deck.Location = new System.Drawing.Point(369, 385);
+            this.current_deck.Name = "current_deck";
+            this.current_deck.Size = new System.Drawing.Size(35, 13);
+            this.current_deck.TabIndex = 1;
+            this.current_deck.Text = "label2";
+            // 
+            // playerLeft
+            // 
+            this.playerLeft.AutoSize = true;
+            this.playerLeft.Location = new System.Drawing.Point(40, 165);
+            this.playerLeft.Name = "playerLeft";
+            this.playerLeft.Size = new System.Drawing.Size(35, 13);
+            this.playerLeft.TabIndex = 2;
+            this.playerLeft.Text = "label3";
+            // 
+            // playerTop
+            // 
+            this.playerTop.AutoSize = true;
+            this.playerTop.Location = new System.Drawing.Point(369, 31);
+            this.playerTop.Name = "playerTop";
+            this.playerTop.Size = new System.Drawing.Size(35, 13);
+            this.playerTop.TabIndex = 3;
+            this.playerTop.Text = "label4";
+            // 
+            // playerRight
+            // 
+            this.playerRight.AutoSize = true;
+            this.playerRight.Location = new System.Drawing.Point(697, 156);
+            this.playerRight.Name = "playerRight";
+            this.playerRight.Size = new System.Drawing.Size(35, 13);
+            this.playerRight.TabIndex = 4;
+            this.playerRight.Text = "label5";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 54);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Gapa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Gapa";
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.playerRight);
+            this.Controls.Add(this.playerTop);
+            this.Controls.Add(this.playerLeft);
+            this.Controls.Add(this.current_deck);
+            this.Controls.Add(this.current_player);
+            this.Name = "Gapa";
+            this.Text = "Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label current_player;
+        private System.Windows.Forms.Label current_deck;
+        private System.Windows.Forms.Label playerLeft;
+        private System.Windows.Forms.Label playerTop;
+        private System.Windows.Forms.Label playerRight;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Gapa.cs
+++ Gapa.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,17 +21,20 @@ namespace Kasyno
         private List<string> player2 = new List<string>();
         private List<string> player3 = new List<string>();
         private List<string> player4 = new List<string>();
+        private int round = 1;
         public Gapa()
         {
             InitializeComponent();
             createDeck();
             shuffleDeck();
             division_players();
+            gameplay();
         }
 
         private void createDeck()
         {
-            List<string> suits = new List<string> { "Trefl", "Karo", "Kier", "Pik" };
+
+            List<string> suits = new List<string> { "♣", "♦", "♥", "♠" };
             List<string> values = new List<string> { "2", "3", "4", "5", "6", "7", "8", "9", "10", "W", "D", "K", "A" };
             foreach (string suit in suits)
             {
@@ -58,10 +62,10 @@ namespace Kasyno
 
         private void division_players()
         {
-            for(int i = 0;i < deck.Count; i++)
+            for (int i = 0; i < deck.Count; i++)
             {
                 int num = i % 4;
-                if(num == 0)
+                if (num == 0)
                 {
                     player1.Add(deck[i]);
                     continue;
@@ -83,4 +87,74 @@ namespace Kasyno
                 }
             }
         }
+
+        void showDeck(int playerNumber)
+        {
+            string cards = "";
+            if (playerNumber == 1)
+            {
+                foreach(string card in player1)
+                {
+                    cards += card +" , ";
+                }
+                playerLeft.Text = "Player 4";
+                playerRight.Text = "Player 2";
+                playerTop.Text = "Player 3";
+                current_player.Text = "Player " + playerNumber;
+                current_deck.Text = cards;
+                current_deck.Location = new Point((this.ClientSize.Width - current_deck.Width) / 2, 382 - current_deck.Height / 2);
+            }
+            else if (playerNumber == 2)
+            {
+                foreach (string card in player2)
+                {
+                    cards += card + " , ";
+                }
+                playerLeft.Text = "Player 1";
+                playerRight.Text = "Player 3";
+                playerTop.Text = "Player 4";
+                current_player.Text = "Player " + playerNumber;
+                current_deck.Text = cards;
+            }
+            else if(playerNumber == 3)
+            {
+                foreach (string card in player3)
+                {
+                    cards += card + " , ";
+                }
+                playerLeft.Text = "Player 2";
+                playerRight.Text = "Player 4";
+                playerTop.Text = "Player 1";
+                current_player.Text = "Player " + playerNumber;
+                current_deck.Text = cards;
+            }
+            else if (playerNumber == 4)
+            {
+                foreach (string card in player4)
+                {
+                    cards += card + " , ";
+                }
+                playerLeft.Text = "Player 3";
+                playerRight.Text = "Player 1";
+                playerTop.Text = "Player 2";
+                current_player.Text = "Player " + playerNumber;
+                current_deck.Text = cards;
+            }       
+        }
+
+        void gameplay()
+        {
+            showDeck(round);
+            round++;
+            if(round == 5)
+            {
+                round = 1;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gameplay();
+        }
+    }
 }
