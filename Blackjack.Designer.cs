f262524fcab88f28aacc126600d0ec4651209739
Michal Walach <michal.walach111@gmail.com>
2024-05-05 01:37:50 +0200

    sorry Adam

--- Blackjack.Designer.cs
+++ Blackjack.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = Properties.Resources.Blackjack_background;
+            // this.BackgroundImage = Properties.Resources.Blackjack_background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Blackjack";
