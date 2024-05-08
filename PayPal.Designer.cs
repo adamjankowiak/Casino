f9d103e8841920908e4719f3f93010c8eef90f08
Michal Walach <michal.walach111@gmail.com>
2024-05-09 00:42:55 +0200

    paypal

---
+++ PayPal.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Kasyno
+{
+    partial class PayPal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buy_100 = new System.Windows.Forms.Button();
+            this.buy_500 = new System.Windows.Forms.Button();
+            this.buy_1000 = new System.Windows.Forms.Button();
+            this.buy_5000 = new System.Windows.Forms.Button();
+            this.buy_10000 = new System.Windows.Forms.Button();
+            this.label_100 = new System.Windows.Forms.Label();
+            this.label_500 = new System.Windows.Forms.Label();
+            this.label_1000 = new System.Windows.Forms.Label();
+            this.label_5000 = new System.Windows.Forms.Label();
+            this.label_10000 = new System.Windows.Forms.Label();
+            this.cost_100 = new System.Windows.Forms.Label();
+            this.cost_500 = new System.Windows.Forms.Label();
+            this.cost_1000 = new System.Windows.Forms.Label();
+            this.cost_5000 = new System.Windows.Forms.Label();
+            this.cost_10000 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buy_100
+            // 
+            this.buy_100.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buy_100.Location = new System.Drawing.Point(116, 146);
+            this.buy_100.Name = "buy_100";
+            this.buy_100.Size = new System.Drawing.Size(88, 36);
+            this.buy_100.TabIndex = 0;
+            this.buy_100.Text = "BUY";
+            this.buy_100.UseVisualStyleBackColor = true;
+            // 
+            // buy_500
+            // 
+            this.buy_500.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buy_500.Location = new System.Drawing.Point(316, 146);
+            this.buy_500.Name = "buy_500";
+            this.buy_500.Size = new System.Drawing.Size(88, 36);
+            this.buy_500.TabIndex = 1;
+            this.buy_500.Text = "BUY";
+            this.buy_500.UseVisualStyleBackColor = true;
+            // 
+            // buy_1000
+            // 
+            this.buy_1000.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buy_1000.Location = new System.Drawing.Point(530, 146);
+            this.buy_1000.Name = "buy_1000";
+            this.buy_1000.Size = new System.Drawing.Size(88, 36);
+            this.buy_1000.TabIndex = 2;
+            this.buy_1000.Text = "BUY";
+            this.buy_1000.UseVisualStyleBackColor = true;
+            // 
+            // buy_5000
+            // 
+            this.buy_5000.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buy_5000.Location = new System.Drawing.Point(216, 344);
+            this.buy_5000.Name = "buy_5000";
+            this.buy_5000.Size = new System.Drawing.Size(88, 36);
+            this.buy_5000.TabIndex = 3;
+            this.buy_5000.Text = "BUY";
+            this.buy_5000.UseVisualStyleBackColor = true;
+            // 
+            // buy_10000
+            // 
+            this.buy_10000.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buy_10000.Location = new System.Drawing.Point(422, 344);
+            this.buy_10000.Name = "buy_10000";
+            this.buy_10000.Size = new System.Drawing.Size(88, 36);
+            this.buy_10000.TabIndex = 4;
+            this.buy_10000.Text = "BUY";
+            this.buy_10000.UseVisualStyleBackColor = true;
+            // 
+            // label_100
+            // 
+            this.label_100.AutoSize = true;
+            this.label_100.Location = new System.Drawing.Point(116, 86);
+            this.label_100.Name = "label_100";
+            this.label_100.Size = new System.Drawing.Size(60, 13);
+            this.label_100.TabIndex = 5;
+            this.label_100.Text = "100 tokens";
+            // 
+            // label_500
+            // 
+            this.label_500.AutoSize = true;
+            this.label_500.Location = new System.Drawing.Point(313, 86);
+            this.label_500.Name = "label_500";
+            this.label_500.Size = new System.Drawing.Size(60, 13);
+            this.label_500.TabIndex = 6;
+            this.label_500.Text = "500 tokens";
+            // 
+            // label_1000
+            // 
+            this.label_1000.AutoSize = true;
+            this.label_1000.Location = new System.Drawing.Point(527, 86);
+            this.label_1000.Name = "label_1000";
+            this.label_1000.Size = new System.Drawing.Size(66, 13);
+            this.label_1000.TabIndex = 7;
+            this.label_1000.Text = "1000 tokens";
+            // 
+            // label_5000
+            // 
+            this.label_5000.AutoSize = true;
+            this.label_5000.Location = new System.Drawing.Point(213, 284);
+            this.label_5000.Name = "label_5000";
+            this.label_5000.Size = new System.Drawing.Size(66, 13);
+            this.label_5000.TabIndex = 8;
+            this.label_5000.Text = "5000 tokens";
+            // 
+            // label_10000
+            // 
+            this.label_10000.AutoSize = true;
+            this.label_10000.Location = new System.Drawing.Point(419, 284);
+            this.label_10000.Name = "label_10000";
+            this.label_10000.Size = new System.Drawing.Size(72, 13);
+            this.label_10000.TabIndex = 9;
+            this.label_10000.Text = "10000 tokens";
+            // 
+            // cost_100
+            // 
+            this.cost_100.AutoSize = true;
+            this.cost_100.Location = new System.Drawing.Point(116, 99);
+            this.cost_100.Name = "cost_100";
+            this.cost_100.Size = new System.Drawing.Size(34, 13);
+            this.cost_100.TabIndex = 10;
+            this.cost_100.Text = "0.99$";
+            // 
+            // cost_500
+            // 
+            this.cost_500.AutoSize = true;
+            this.cost_500.Location = new System.Drawing.Point(313, 99);
+            this.cost_500.Name = "cost_500";
+            this.cost_500.Size = new System.Drawing.Size(34, 13);
+            this.cost_500.TabIndex = 11;
+            this.cost_500.Text = "2.99$";
+            // 
+            // cost_1000
+            // 
+            this.cost_1000.AutoSize = true;
+            this.cost_1000.Location = new System.Drawing.Point(527, 99);
+            this.cost_1000.Name = "cost_1000";
+            this.cost_1000.Size = new System.Drawing.Size(34, 13);
+            this.cost_1000.TabIndex = 12;
+            this.cost_1000.Text = "4.99$";
+            // 
+            // cost_5000
+            // 
+            this.cost_5000.AutoSize = true;
+            this.cost_5000.Location = new System.Drawing.Point(213, 297);
+            this.cost_5000.Name = "cost_5000";
+            this.cost_5000.Size = new System.Drawing.Size(40, 13);
+            this.cost_5000.TabIndex = 13;
+            this.cost_5000.Text = "19.99$";
+            // 
+            // cost_10000
+            // 
+            this.cost_10000.AutoSize = true;
+            this.cost_10000.Location = new System.Drawing.Point(419, 297);
+            this.cost_10000.Name = "cost_10000";
+            this.cost_10000.Size = new System.Drawing.Size(40, 13);
+            this.cost_10000.TabIndex = 14;
+            this.cost_10000.Text = "29.99$";
+            // 
+            // PayPal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cost_10000);
+            this.Controls.Add(this.cost_5000);
+            this.Controls.Add(this.cost_1000);
+            this.Controls.Add(this.cost_500);
+            this.Controls.Add(this.cost_100);
+            this.Controls.Add(this.label_10000);
+            this.Controls.Add(this.label_5000);
+            this.Controls.Add(this.label_1000);
+            this.Controls.Add(this.label_500);
+            this.Controls.Add(this.label_100);
+            this.Controls.Add(this.buy_10000);
+            this.Controls.Add(this.buy_5000);
+            this.Controls.Add(this.buy_1000);
+            this.Controls.Add(this.buy_500);
+            this.Controls.Add(this.buy_100);
+            this.Name = "PayPal";
+            this.Text = "PayPal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buy_100;
+        private System.Windows.Forms.Button buy_500;
+        private System.Windows.Forms.Button buy_1000;
+        private System.Windows.Forms.Button buy_5000;
+        private System.Windows.Forms.Button buy_10000;
+        private System.Windows.Forms.Label label_100;
+        private System.Windows.Forms.Label label_500;
+        private System.Windows.Forms.Label label_1000;
+        private System.Windows.Forms.Label label_5000;
+        private System.Windows.Forms.Label label_10000;
+        private System.Windows.Forms.Label cost_100;
+        private System.Windows.Forms.Label cost_500;
+        private System.Windows.Forms.Label cost_1000;
+        private System.Windows.Forms.Label cost_5000;
+        private System.Windows.Forms.Label cost_10000;
+    }
+}
